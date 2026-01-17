9bbf64b8875bcd3eec92f70f2fbffe70a1aa1243
ItsHarshXD <msharshraj@gmail.com>
2026-01-17 18:56:26 +0530

    feat: Initialize OutfitsPlus plugin with Gradle wrapper and basic structure
    - Added gradle-wrapper.properties for Gradle configuration.
    - Included gradlew and gradlew.bat for script execution on Unix and Windows.
    - Created settings.gradle to define the root project name as 'OutfitsPlus'.
    - Implemented OutfitsPlus main class with PacketEvents integration.
    - Added plugin.yml with metadata and dependencies for the OutfitsPlus plugin.

---
+++ src/main/java/dev/harsh/plugin/outfitsplus/OutfitsPlus.java
@@ -0,0 +1,25 @@
+package dev.harsh.plugin.outfitsplus;
+
+import com.github.retrooper.packetevents.PacketEvents;
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OutfitsPlus extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        PacketEvents.getAPI().load();
+    }
+
+    @Override
+    public void onEnable() {
+        PacketEvents.getAPI().init();
+
+    }
+
+    @Override
+    public void onDisable() {
+        PacketEvents.getAPI().terminate();
+    }
+}
